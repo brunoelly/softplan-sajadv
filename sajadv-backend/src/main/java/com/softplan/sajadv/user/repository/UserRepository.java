5069a9901c209f53ddc3ee3c155fc85cc007cc9a
brunoelly <brunoelly@outlook.com>
2023-06-19 16:36:19 -0300

    Fix UserRepository imports

--- sajadv-backend/src/main/java/com/softplan/sajadv/user/repository/UserRepository.java
+++ sajadv-backend/src/main/java/com/softplan/sajadv/user/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.softplan.sajadv.user.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import com.softplan.sajadv.user.model.User;
 
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
